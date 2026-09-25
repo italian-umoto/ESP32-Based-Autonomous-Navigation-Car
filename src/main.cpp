4dcc25ebb7f795846c12f7c753fcc35d302e8232
justin zhang <justinzhang2021@gmail.com>
2026-09-25 00:56:05 -0400

    collision detect 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,57 @@
 #include <Arduino.h>
 
+const int IR_LED_PIN = 5;
 const int SENSOR_PIN = 4;
-const int RED_LED_PIN = 5;
-const int GREEN_LED_PIN = 6;
-const int BLUE_LED_PIN = 7;
-const int NUM_SAMPLES = 16;
-const int DARK_THRESHOLD = 50;
-const float COLOR_DOMINANCE = 1.25f; // color winner margin
-const int DELAY_MS = 10;
+
+const int COLLISION_THRESHOLD = 900;
+const int NUM_SAMPLES = 4;
 
 int readAverage() {
-    long total = 0;
-    for (int i = 0; i < NUM_SAMPLES; ++i) {
-        total += analogRead(SENSOR_PIN);
-        delayMicroseconds(100);
-    }
-    return total / NUM_SAMPLES;
-}
+    long sum = 0;
 
-// Sets one of R/G/B high and gets the reading minus the ambient light
-int readReflection(int ledPin, int ambient) {
-    digitalWrite(ledPin, HIGH);
-    delay(DELAY_MS);
-    const int illuminated = readAverage();
-    digitalWrite(ledPin, LOW);
-    return max(0, illuminated - ambient);
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        sum += analogRead(SENSOR_PIN);
+    }
+
+    return sum / NUM_SAMPLES;
 }
 
 void setup() {
     Serial.begin(115200);
-    pinMode(RED_LED_PIN, OUTPUT);
-    pinMode(GREEN_LED_PIN, OUTPUT);
-    pinMode(BLUE_LED_PIN, OUTPUT);
 
-    // return values between 0 and 4095
+    pinMode(IR_LED_PIN, OUTPUT);
+    digitalWrite(IR_LED_PIN, LOW);
+
     analogReadResolution(12);
-
-    // configure ADC to measure higher voltages
     analogSetPinAttenuation(SENSOR_PIN, ADC_11db);
+
     delay(1000);
 }
 
 void loop() {
-    digitalWrite(RED_LED_PIN, LOW);
-    digitalWrite(GREEN_LED_PIN, LOW);
-    digitalWrite(BLUE_LED_PIN, LOW);
+    digitalWrite(IR_LED_PIN, LOW);
+    delayMicroseconds(300);
+    int background1 = readAverage();
+    
+    digitalWrite(IR_LED_PIN, HIGH);
+    delayMicroseconds(300);
+    int illuminated = readAverage();
+    
+    digitalWrite(IR_LED_PIN, LOW);
+    delayMicroseconds(300);
+    int background2 = readAverage();
+    
+    int background = (background1 + background2) / 2;
+    int reflected = illuminated - background;
+    bool imminent_collision = false;
+    if (reflected > COLLISION_THRESHOLD) imminent_collision = true;
 
-    const int ambient = readAverage();
-    delay(DELAY_MS);
-    const int green = readReflection(GREEN_LED_PIN, ambient);
-    delay(DELAY_MS);
-    const int red = readReflection(RED_LED_PIN, ambient);
-    delay(DELAY_MS);
-    const int blue = readReflection(BLUE_LED_PIN, ambient);
-
-    const int brightest = max(red, max(green, blue));
-    const char* color = "white";
-    if (brightest < DARK_THRESHOLD) color = "dark";
-    else if (red > green * COLOR_DOMINANCE && red > blue * COLOR_DOMINANCE) color = "red";
-    else if (green > red * COLOR_DOMINANCE && green > blue * COLOR_DOMINANCE) color = "green";
-    else if (blue > red * COLOR_DOMINANCE && blue > green * COLOR_DOMINANCE) color = "blue";
-    Serial.print("R: "); Serial.print(red);
-    Serial.print("  G: "); Serial.print(green);
-    Serial.print("  B: "); Serial.print(blue);
-    Serial.print("  color: "); Serial.println(color);
-    delay(100);
+    Serial.printf(
+        "background: %4d  illuminated: %4d  reflected: %4d  %s\n",
+        background,
+        illuminated,
+        reflected,
+        imminent_collision ? "ABOUT TO COLLIDE" : "SAFE"
+    );
+    delay(50);
 }
